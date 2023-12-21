171b413f4e2c6d2beaedebafc7d7c0b6cb6394a9
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2023-12-21 20:40:12 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,61 +6,58 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
-require 'open-uri'
 
 
 User.destroy_all
 Post.destroy_all
 Comment.destroy_all
 
+image_links = [
+  "https://images.pexels.com/photos/4050347/pexels-photo-4050347.jpeg?cs=srgb&dl=pexels-vlada-karpovich-4050347.jpg&fm=jpg",
+  "https://img.freepik.com/photos-premium/annuaire-telephonique-portable-table-bois_36923-1057.jpg?size=626&ext=jpg",
+  "https://img.freepik.com/photos-premium/photo-retour-ecole_896782-15587.jpg",
+  "https://st2.depositphotos.com/4107269/7705/i/450/depositphotos_77053677-stock-photo-journalist-working-on-his-new.jpg",
+  "https://st3.depositphotos.com/8205786/37240/i/450/depositphotos_372406812-stock-photo-two-girls-in-a-white.jpg",
+  "https://st3.depositphotos.com/12548352/16360/i/450/depositphotos_163601446-stock-photo-girl-writing-in-notebook.jpg",
+  "https://images.pexels.com/photos/4468150/pexels-photo-4468150.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4468150.jpg&fm=jpg",
+  "https://media.istockphoto.com/id/1189962383/photo/contemporary-young-women-working-in-cafe.jpg?s=612x612&w=0&k=20&c=b1rmEdKih4VpOPJrQH1Ki1wE6wkvjmgFSPaPlew_o5w=",
+  "https://media.istockphoto.com/id/939031194/fr/photo/portrait-de-souriant-freelance-intelligente-en-v%C3%AAtements-d%C3%A9contract%C3%A9s-et-de-spectacles-en.jpg?s=612x612&w=0&k=20&c=oLsihD45jLWSNTvk-yE0Ag-8_oG6Qb1VSx-MMub-IJE=",
+  "https://st4.depositphotos.com/2208684/23747/i/450/depositphotos_237473972-stock-photo-portrait-shot-of-businessman-looking.jpg"
+]
+
+compteur = 0
 
 users = []
-5.times do
-  user = User.create!(
+
+10.times do
+  user = User.create(
     email: Faker::Internet.email,
-    password: "password",
+    password: "azerty",
     nickname: Faker::Internet.username,
     description: Faker::Lorem.paragraphs(number: 10).join("\n")
   )
-
-  file_path = Rails.root.join("app/assets/images/user.jpeg")
-  file = File.open(file_path)
-  user.photo.attach(io: file, filename: 'user.jpeg', content_type: 'image/jpeg')
+  user.save!
   users << user
-end
 
+  post = Post.create(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.sentences(number: 5).join(" "),
+    content: Faker::Lorem.sentences(number: 20).join("\n"),
+    url: Faker::Internet.url,
+    user_id: user.id,
+    post_photo: image_links[compteur]
+  )
+  post.save!
+  compteur += 1
 
-puts "Users finished"
-
-# Create posts
-posts = []
-users.each do |user|
   2.times do
-    post = user.posts.create!(
-      title: Faker::Lorem.sentence,
-      description: Faker::Lorem.sentences(number: 5).join(" "),
-      content: Faker::Lorem.sentences(number: 20).join("\n"),
-      url: Faker::Internet.url
+    Comment.create!(
+      content: Faker::Lorem.sentence,
+      user_id: user.id,
+      post_id: post.id
     )
-    post.photo.attach(io: File.open("app/assets/images/article.jpeg"), filename: 'article.jpeg', content_type: 'image/jpeg')
-    posts << post
   end
 end
-puts "Posts finished"
 
-# Create comments
-comments = []
-users.each do |user|
-  posts.each do |post|
-    2.times do
-      comments << Comment.create!(
-        content: Faker::Lorem.sentence,
-        user: user,
-        post: post
-      )
-    end
-  end
-end
-puts "Comments finished"
-
+puts "Users, Posts, and Comments finished"
 puts "100% finished"
